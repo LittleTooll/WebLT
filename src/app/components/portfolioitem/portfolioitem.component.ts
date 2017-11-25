be431254fb301abf7a0a336dc66af4e4944e33f2
LittleTooll <littletooll.reload@gmail.com>
2017-11-25 18:28:17 +0100

    About y portfolioitem

---
+++ src/app/components/portfolioitem/portfolioitem.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolioitem',
+  templateUrl: './portfolioitem.component.html'
+})
+export class PortfolioitemComponent {
+
+}
