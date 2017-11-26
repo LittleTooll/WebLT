e059f30e50bd215460fd421c111a3973f2fc1aeb
LittleTooll <littletooll.reload@gmail.com>
2017-11-26 11:38:00 +0100

    Servicio firebase about

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,16 +7,30 @@ export class InformacionService {
   info:any = {};
   cargada:boolean = false;
 
+  equipo:any=[];
+  cargada_sobre_nosotros:boolean = false;
+
   constructor( public http:Http ) { 
     
-    this.http.get("assets/data/info.pagina.json")
-      .subscribe( data => {
-          console.log(data.json());
-          this.cargada = true;
-          this.info = data.json();
-        });
+    this.carga_info();
+    this.carga_sobre_nosotros();
 
   }
 
+  public carga_info(){
+    this.http.get("assets/data/info.pagina.json")
+        .subscribe( data => {
+            this.cargada = true;
+            this.info = data.json();
+          });
+  }
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://paginaweb-d3ce1.firebaseio.com/equipo.json")
+        .subscribe( data => {
+            this.cargada_sobre_nosotros = true;
+            this.equipo = data.json();
+          });
+  }
 
 }
