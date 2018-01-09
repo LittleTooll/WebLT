9dfc653d02952e37a102ac7c8a4742061b27271e
LittleTooll <littletooll.reload@gmail.com>
2018-01-09 23:36:22 +0100

    Versión de estable de la web de littletooll

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,10 +5,38 @@ import { Http } from "@angular/http";
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean = false;
 
   constructor( public http:Http ) { 
     this.cargar_productos();
+  }
+
+  public buscar_producto (termino:string){
+    //console.log("Buscando producto");
+    //console.log(this.productos.length);
+
+    if (this.productos.length === 0){
+      this.cargar_productos().then(()=> {
+        // terminó la carga
+        this.filtrar_productos(termino);
+      });
+    } else {
+      this.filtrar_productos(termino);
+    }
+    
+  }
+
+  private filtrar_productos (termino:string){
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod =>{
+      if (prod.categoria.toLowerCase().indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0){
+        this.productos_filtrado.push (prod);
+      }
+    })
   }
 
   public cargar_producto(cod:string){
@@ -20,14 +48,19 @@ export class ProductosService {
   public cargar_productos(){
       this.cargando = true;
 
-      this.http.get ('https://paginaweb-d3ce1.firebaseio.com/productos_idx.json')
+      let promesa = new Promise((resolve, reject) => {
+        this.http.get ('https://paginaweb-d3ce1.firebaseio.com/productos_idx.json')
         .subscribe( res => {
           
           console.log(res.json());
 
           this.cargando = false;
           this.productos = res.json();  
+          resolve();
         });
+      });
+      
+      return promesa;
   }
 
 }
